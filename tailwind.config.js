a560da1651fbfd1b0de7bb5facb7e22a48f7537d
Onur Ozer <onurozer@gmail.com>
2022-06-20 17:33:27 +0200

    making it right

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["futura-pt-condensed", ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        xxl: ["12rem", "1"],
+      },
+    },
+  },
+};
